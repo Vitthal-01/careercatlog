70e339fe774fd4e211cf886cba0fff38a18c95e6
Vitthal-01 <166918699+Vitthal-01@users.noreply.github.com>
2026-09-09 08:59:31 +0530

    Update WhatsApp number in popup.js

--- popup.js
+++ popup.js
@@ -16,7 +16,7 @@
 
 (function () {
   // ---- CONFIG -------------------------------------------------
-  var WHATSAPP_NUMBER = "91XXXXXXXXXX"; // <-- replace with your number, country code + number, no + or spaces
+  var WHATSAPP_NUMBER = "919168796201"; // <-- replace with your number, country code + number, no + or spaces
   var SHOW_DELAY_MS = 3000;
   var SESSION_KEY = "cc_popup_shown_v1";
 
